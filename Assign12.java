09f1248fa2fe56d5e3e3039ea84ccf79ef7ed2c3
Rajesh-1234 <68465886+Rajesh-1234@users.noreply.github.com>
2020-09-22 00:38:52 +0530

    Add files via upload

---
+++ Assign12.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+   
+ public class Assign12
+
+
+{
+
+
+     public static void main(String args[])
+
+
+{
+
+          Scanner scanner = new Scanner(System.in);
+
+
+
+}
+
+
+  }
---
+++ Assign14.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+  
+  public class Assign14
+
+
+{
+
+
+     public static void main(String args[])
+
+
+{
+  
+        Scanner scanner = new Scanner(System.in);
+ 
+ 
+         System.out.print("\nEnter the year = ");
+     
+     int year = scanner.nextInt();
+
+   
+    if  (year%4==0 && year%100!=0 || year%400 == 0)
+
+  
+ {
+
+    
+    
+  System.out.println("\nGiven year " +year+" is leap year");
+  
+      
+  }
+
+
+    else
+
+
+         {
+
+            
+        System.out.println("\nGiven year " +year+" is not leap year");
+
+      
+ }
+
+
+        
+
+
+ }
+
+
+
+ }
